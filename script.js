c606d1e7daf806554a3e989f0c28f051ff74ff3d
zeynepkrmc <m.zeynephnm@gmail.com>
2023-07-12 11:02:48 +0300

    password validations added

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const form = document.getElementById('form');
 const fname = document.getElementById('fname');
 const lname = document.getElementById('lname');
@@ -6,45 +7,64 @@ const password = document.getElementById('password');
 const repassword = document.getElementById('repassword');
 
 function error(input, message) {
-    input.className = 'form-control is-invalid'
+    input.className = 'form-control is-invalid';
     const div = input.nextElementSibling;
     div.innerText =  message;
     div.className = 'invalid-feedback';
 }
+
 function success(input) {
-    input.className = 'form-control is-valid'
+    input.className = 'form-control is-valid';
 }
 
+function validatePassword() {
+    if (password.value !== repassword.value) {
+        error(repassword, 'Passwords do not match');
+    } else {
+        success(repassword);
+    }
+}
 
-form.addEventListener('submit',function(e){
-    
-    if(fname.value === '') {
-        error(fname, ' first name needed');
-    }else {
-        success(fname)
-    }
-    if(lname.value === '') {
-        error(lname, 'last name needed');
-    }else {
-        success(lname)
-    }
-    if(email.value === '') {
-        error(email,'email needed');
-    }else {
-        success(email)
-    }
-    if(password.value === '') {
-        error(password, 'password needed');
-    }else {
-        success(password)
-    }
-    if(repassword.value === '') {
-        error(repassword, 'type password again');
-    }else {
-        success(repassword)
-    }
-    
+function validateEmail() {
+    const emailValue = email.value;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    
+    if (emailValue === '') {
+        error(email, 'Email is required');
+    } else if (!emailRegex.test(emailValue)) {
+        error(email, 'Invalid email format');
+    } else {
+        success(email);
+    }
+}
+
+function validatePasswordRequirements() {
+    const passwordValue = password.value;
+    const passwordRegex = /^(?=.*[a-zA-Z]).{8,}$/;
+
+    if (!passwordRegex.test(passwordValue)) {
+        error(password, 'Password must be at least 8 characters long and contain at least one letter');
+    } else {
+        success(password);
+    }
+}
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    if (fname.value === '') {
+        error(fname, 'First name is required');
+    } else {
+        success(fname);
+    }
+
+    if (lname.value === '') {
+        error(lname, 'Last name is required');
+    } else {
+        success(lname);
+    }
+
+    validateEmail();
+    validatePasswordRequirements();
+    validatePassword();
 });
-
